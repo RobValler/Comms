5aaef8370256c3f7c8c7c4ad26c073a4731e7b37
R V <r.valler@yahoo.co.uk>
2021-04-22 00:31:15 +0200

    Added generic folder

---
+++ common/common.h
@@ -0,0 +1,18 @@
+/*****************************************************************
+ * Copyright (C) 2017-2021 Robert Valler - All rights reserved.
+ *
+ * This file is part of the project: StarterApp
+ *
+ * This project can not be copied and/or distributed
+ * without the express permission of the copyright holder
+ *****************************************************************/
+
+#pragma once
+
+enum EProtocolType : unsigned int
+{
+    ENone = 0,
+    ETCTPIP,
+    EPOSIX_MQ
+};
+
