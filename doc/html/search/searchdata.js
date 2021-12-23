805d8129bac2dec1025a6818ad3d4a7c34ad6062
R V <r.valler@yahoo.co.uk>
2021-12-24 00:37:27 +0100

    Fixed diagrams and removed latex
    Latex is no longer enabled in the doxygen config
    fixed class diagrams with installing dot and graphviz

--- doc/html/search/searchdata.js
+++ doc/html/search/searchdata.js
@@ -1,12 +1,27 @@
 var indexSectionsWithContent =
 {
+  0: "_cimprst~",
+  1: "_cipst",
+  2: "c~",
+  3: "m",
+  4: "cmrt"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "variables",
+  4: "pages"
 };
 
 var indexSectionLabels =
 {
+  0: "All",
+  1: "Classes",
+  2: "Functions",
+  3: "Variables",
+  4: "Pages"
 };
 
