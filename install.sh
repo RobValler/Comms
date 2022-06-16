bc4668a63c7a18fd069a143e1ad09ff4e65cd51e
R V <r.valler@yahoo.co.uk>
2022-06-16 23:33:19 +0200

    update to install script

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ mkdir -p build || true
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j16
-test/./comms_test_server &
-test/./comms_test_client 
+#test/./comms_test_server &
+#test/./comms_test_client 
 sudo cmake --install .
 cd ..
 
