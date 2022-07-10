9b7b747d4e298ca1517545cb3a0fc6426dcdf234
R V <r.valler@yahoo.co.uk>
2022-07-10 11:02:09 +0200

    Added release and debug build folders
    Fixed some issues

--- CommsConfig.cmake
+++
@@ -1,4 +0,0 @@
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/Comms-targets.cmake)
-get_filename_component(COMMS_INCLUDE_DIRS "${SELF_DIR}/include" ABSOLUTE)
-
--- install.sh
+++ install.sh
@@ -3,11 +3,18 @@ set -e
 rm -rf build || true
 mkdir -p build
 cd build
-conan install .. --build missing
-cmake .. -DCMAKE_BUILD_TYPE=Release
+mkdir -p release
+mkdir -p debug
+#build release
+cd release
+conan install ../.. --build missing
+cmake ../.. -DCMAKE_BUILD_TYPE=Release
 make -j16
-#test/./comms_test_server &
-#test/./comms_test_client 
 sudo cmake --install .
 cd ..
-
+#build debug
+cd debug
+conan install ../.. --build missing
+cmake ../.. -DCMAKE_BUILD_TYPE=Debug
+make -j16
+cd ../..
